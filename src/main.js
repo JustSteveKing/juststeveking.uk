2718dd880ce81387e8667c06ef848a49fc9c7b37
Steve McDougall <juststeveking@gmail.com>
2020-04-23 09:42:38 +0100

    Fixing live chat window issue

--- src/main.js
+++ src/main.js
@@ -11,14 +11,3 @@ export default function (Vue, { router, head, isClient }) {
   head.bodyAttrs = { class: 'antialiased' };
 }
 
-window.$crisp = [];
-window.CRISP_WEBSITE_ID = "491dbe8b-afd0-4f6e-942c-68daba40ce11";
-(function () {
-  let d = document;
-  let s = d.createElement("script");
-  s.src = "https://client.crisp.chat/l.js";
-  s.async = 1;
-  d.getElementsByTagName("head")[0].appendChild(s);
-})();
-
-
