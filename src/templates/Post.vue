4afcb707425365583ee8be7ae619f784f4688975
Steve McDougall <juststeveking@gmail.com>
2020-05-29 20:26:54 +0100

    Styling markdown content better

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -129,3 +129,14 @@ query Post ($path: String) {
   }
 }
 </page-query>
+
+<style>
+.markdown > p {
+  margin: 1rem 0 1rem 0;
+}
+
+.markdown > pre {
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  border-radius: 0.375rem;
+}
+</style>
