c816ab904d5b264bc20ba2ae490c8c51e77b7bb9
Steve McDougall <juststeveking@gmail.com>
2020-06-04 09:50:41 +0100

    Using prism theme through a cdn

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import DefaultLayout from '~/layouts/Default.vue';
 import SiteHeader from '~/components/SiteHeader.vue';
 import SiteFooter from '~/components/SiteFooter.vue';
 
-import 'prismjs/themes/prism-tomorrow.css'
-
 export default function (Vue, { router, head, isClient }) {
   Vue.component('Layout', DefaultLayout);
   Vue.component('site-header', SiteHeader);
@@ -11,5 +9,11 @@ export default function (Vue, { router, head, isClient }) {
   Vue.use(require('vue-moment'));
   head.htmlAttrs = { lang: 'en', class: 'h-full' };
   head.bodyAttrs = { class: 'antialiased' };
+
+  // add prismjs theme
+  head.link.push({
+    rel: 'stylesheet',
+    href: 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.20.0/themes/prism-tomorrow.min.css'
+  });
 }
 
