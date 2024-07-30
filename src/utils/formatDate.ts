cfadcd64a1d7b57499bc44eabe4022d2981efeb3
Steve McDougall <juststevemcd@gmail.com>
2024-07-30 10:17:34 +0100

    Improving the site and adding content

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,7 +1,13 @@
-export default function formatDate(date: Date) {
+export function formatDate(date: Date) {
   return new Intl.DateTimeFormat("en-GB", {
     year: "numeric",
     month: "long",
     day: "numeric"
   }).format(date);
 }
+
+export function datetime(date: Date) {
+  return new Intl.DateTimeFormat("en-GB", {
+
+  }).format(date);
+};
